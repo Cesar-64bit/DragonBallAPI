a4ede19b17adba6cb59717e02631dc5d12475493
Cesar Bernal <mrcode.hack28@gmail.com>
2024-04-11 20:06:34 -0700

    Ajuste application.properties

--- src/main/java/com/cesar/dragonball/backend/api/repository/UniversoRepository.java
+++ src/main/java/com/cesar/dragonball/backend/api/repository/UniversoRepository.java
@@ -4,6 +4,5 @@ import com.cesar.dragonball.backend.api.model.Universo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UniversoRepository extends JpaRepository<Universo, Long> {
 }
