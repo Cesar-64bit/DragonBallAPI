9c3265a86aa72556274800ec81096018db1f4c4b
Cesar Bernal <mrcode.hack28@gmail.com>
2024-04-11 18:02:30 -0700

    Creacion de la clase Repository

---
+++ src/main/java/com/cesar/dragonball/backend/api/repository/UniversoRepository.java
@@ -0,0 +1,9 @@
+package com.cesar.dragonball.backend.api.repository;
+
+import com.cesar.dragonball.backend.api.model.Universo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UniversoRepository extends JpaRepository<Universo, Integer> {
+}
