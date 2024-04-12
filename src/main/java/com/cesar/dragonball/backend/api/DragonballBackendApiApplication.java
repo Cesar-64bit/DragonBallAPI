6a166e6ba4d3a9292f8c8215da34294ea40870d2
Cesar Bernal <mrcode.hack28@gmail.com>
2024-04-11 17:57:56 -0700

    Inicializando proyecto

---
+++ src/main/java/com/cesar/dragonball/backend/api/DragonballBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.cesar.dragonball.backend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonballBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonballBackendApiApplication.class, args);
+	}
+
+}
