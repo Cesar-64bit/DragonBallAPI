5cae976a1245e88c53584e8dca782fb3ddad8346
Cesar Bernal <mrcode.hack28@gmail.com>
2024-04-17 21:06:24 -0700

    Implementacion de HabilidadRepository

---
+++ src/main/java/com/cesar/dragonball/backend/api/repository/HabilidadRepository.java
@@ -0,0 +1,8 @@
+package com.cesar.dragonball.backend.api.repository;
+
+import com.cesar.dragonball.backend.api.model.Habilidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HabilidadRepository extends JpaRepository<Habilidad, Long> {
+
+}
