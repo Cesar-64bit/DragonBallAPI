9f3174aa9a390258d2ec20fe5c6c4660fc54bc38
Cesar Bernal <mrcode.hack28@gmail.com>
2024-04-18 11:07:49 -0700

    Implementacion de RazaRepository

---
+++ src/main/java/com/cesar/dragonball/backend/api/repository/RazaRepository.java
@@ -0,0 +1,7 @@
+package com.cesar.dragonball.backend.api.repository;
+
+import com.cesar.dragonball.backend.api.model.Raza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RazaRepository extends JpaRepository<Raza, Long> {
+}
