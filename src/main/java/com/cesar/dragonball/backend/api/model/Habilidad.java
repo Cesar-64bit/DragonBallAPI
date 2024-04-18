04ef2895b0eb2b4d89da98460db8ed6bf756a01e
Cesar Bernal <mrcode.hack28@gmail.com>
2024-04-17 21:04:40 -0700

    Implementacion de Modelo Habilidad

---
+++ src/main/java/com/cesar/dragonball/backend/api/model/Habilidad.java
@@ -0,0 +1,23 @@
+package com.cesar.dragonball.backend.api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "habilidad")
+public class Habilidad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 4, max = 50)
+    @Column(name = "nombre", nullable = false, length = 50)
+    private String nombre;
+}
