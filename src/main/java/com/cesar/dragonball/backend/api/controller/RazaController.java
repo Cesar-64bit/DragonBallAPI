53b0d0ecf97a48d7a39d30f4e9a1b47d15ca0095
Cesar Bernal <mrcode.hack28@gmail.com>
2024-04-18 11:45:07 -0700

    Desarrollo de RazaController

---
+++ src/main/java/com/cesar/dragonball/backend/api/controller/RazaController.java
@@ -0,0 +1,62 @@
+package com.cesar.dragonball.backend.api.controller;
+
+import com.cesar.dragonball.backend.api.model.Raza;
+import com.cesar.dragonball.backend.api.service.RazaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class RazaController {
+
+    @Autowired
+    private RazaService razaService;
+
+    @GetMapping("/razas")
+    public List<Raza> index() {
+        return razaService.findAll();
+    }
+
+    @PostMapping("/razas")
+    public ResponseEntity<?> create(@Valid @RequestBody Raza raza, BindingResult result) {
+        Raza razaNew = null;
+
+        Map<String, Object> response = new HashMap<>();
+
+        if(result.hasErrors()) {
+            List<String> errors = result.getFieldErrors()
+                    .stream()
+                    .map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
+                    .collect(Collectors.toList());
+
+            response.put("errors", errors);
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            razaNew = razaService.save(raza);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al realizar el insert en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "La raza ha sido creada con éxito!");
+        response.put("raza", razaNew);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+    }
+}
